2b1f909435720be41ff95e2cd224312a0a74ad6b
Steven Tohme <steventohme59@gmail.com>
2023-12-27 16:24:59 -0500

    add http error handling to factor router

--- attributions_wiki/routers/api/factor_router.py
+++ attributions_wiki/routers/api/factor_router.py
@@ -1,7 +1,7 @@
 """Factor router."""
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from prisma.models import Factor
 from prisma.types import FactorCreateInput, FactorUpdateInput, FactorWhereUniqueInput
@@ -11,7 +11,7 @@ from ...db import db
 router = APIRouter(
     prefix="/factor",
     tags=["factor"],
-    responses={404: {"description": "Not found"}},
+    responses={404: {"description": "Factor not found"}},
 )
 
 
@@ -22,8 +22,11 @@ async def create_factor(factor_data: FactorCreateInput) -> Factor:
     Args: factor_data: FactorCreateInput
     Returns: Factor
     """
-    new_factor: Factor = await db.factor.create(factor_data)
-    return new_factor
+    try:
+        new_factor: Factor = await db.factor.create(factor_data)
+        return new_factor
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.get("/get_all")
@@ -32,8 +35,11 @@ async def get_factors() -> List[Factor]:
 
     Returns: List[Factor]
     """
-    factors: List[Factor] = await db.factor.find_many()
-    return factors
+    try:
+        factors: List[Factor] = await db.factor.find_many()
+        return factors
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/get/{id}")
@@ -45,6 +51,8 @@ async def get_factor_by_id(id: int) -> Factor:
     """
     id_obj: FactorWhereUniqueInput = FactorWhereUniqueInput(id=id)  # type: ignore
     factor: Factor = await db.factor.find_unique_or_raise(id_obj)  # type: ignore
+    if not factor:
+        raise HTTPException(status_code=404, detail="Factor not found")
     return factor
 
 
@@ -57,6 +65,8 @@ async def delete_factor_by_id(id: int) -> Factor | None:
     """
     id_obj: FactorWhereUniqueInput = FactorWhereUniqueInput(id=id)  # type: ignore
     factor: Factor | None = await db.factor.delete(id_obj)  # type: ignore
+    if not factor:
+        raise HTTPException(status_code=404, detail="Factor not found")
     return factor
 
 
@@ -68,5 +78,11 @@ async def update_factor(data: FactorUpdateInput, id: int) -> Factor | None:
     Returns: Factor or None
     """
     id_obj: FactorWhereUniqueInput = FactorWhereUniqueInput(id=id)  # type: ignore
-    factor: Factor | None = await db.factor.update(data=data, where=id_obj)  # type: ignore
-    return factor
+    try:
+
+        factor: Factor | None = await db.factor.update(data=data, where=id_obj)  # type: ignore
+        if not factor:
+            raise HTTPException(status_code=404, detail="Factor not found")
+        return factor
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
